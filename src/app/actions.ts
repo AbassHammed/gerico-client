0de96c5ee23002608bd8b5f46775670216db0947
Abass Hammed <139161544+AbassHammed@users.noreply.github.com>
2024-12-10 23:11:36 +0100

    Decided to upload the file on the front cause i can't find a very secure way to send the file to back - At least here i know what i am doing

---
+++ src/app/actions.ts
@@ -0,0 +1,59 @@
+'use server';
+
+import crypto from 'crypto';
+
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const s3Client = new S3Client({
+  forcePathStyle: false,
+  endpoint: process.env.NEXT_PUBLIC_DO_ENDPOINT!,
+  region: process.env.NEXT_PUBLIC_DO_REGION!,
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_DO_ACCESS_KEY!,
+    secretAccessKey: process.env.NEXT_PUBLIC_DO_SECRET_KEY!,
+  },
+});
+
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
+
+const allowedFileTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'application/pdf'];
+
+type SignedURLResponse = Promise<
+  | { failure?: undefined; success: { signedUrl: string; fileUrl: string } }
+  | { failure: string; success?: undefined }
+>;
+
+type GetSignedURLParams = {
+  fileType: string;
+  fileSize: number;
+  checksum: string;
+};
+
+export const getSignedURL = async ({
+  fileType,
+  fileSize,
+  checksum,
+}: GetSignedURLParams): SignedURLResponse => {
+  if (!allowedFileTypes.includes(fileType)) {
+    return { failure: 'Invalid file type' };
+  }
+
+  const fileName = generateFileName();
+  const command = new PutObjectCommand({
+    Bucket: process.env.NEXT_PUBLIC_DO_BUCKET_NAME!,
+    Key: fileName,
+    ContentType: fileType,
+    ContentLength: fileSize,
+    ChecksumSHA256: checksum,
+    ACL: 'public-read',
+  });
+
+  try {
+    await s3Client.send(command);
+    const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 60 * 60 });
+    return { success: { signedUrl, fileUrl: signedUrl.split('?')[0] } };
+  } catch (error: any) {
+    return { failure: error.message };
+  }
+};
