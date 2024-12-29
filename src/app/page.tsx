47faea765fc5d10fbc69fb805f7e2f0d0d1b1a4a
Anonymous User <anonymous@example.com>
2024-12-29 17:50:34 +0100

    feat(): add functional link to sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import React from 'react';
 import AnimatedTestimonials from '@/components/interfaces/Home/AnimatedTestimonials';
 import CardDefault from '@/components/interfaces/Home/CardDefault';
 import HeroSection from '@/components/interfaces/Home/HeroSection';
-import HomeFooter from '@/components/interfaces/Home/HomeFooter';
-import HomeNavbar from '@/components/interfaces/Home/HomeNavbar';
 // import PromoCard from '@/components/interfaces/Home/PromoCard';
 import PromoCardText from '@/components/interfaces/Home/PromoCardText';
+import Footer from '@/components/shared/Footer';
+// import HomeFooter from '@/components/interfaces/Home/HomeFooter';
+import HomeNavbar from '@/components/shared/HomeNavbar';
 
 export default function Home() {
   return (
@@ -18,7 +19,8 @@ export default function Home() {
       <CardDefault />
       <PromoCardText />
       <AnimatedTestimonials autoplay={true} />
-      <HomeFooter />
+      <Footer />
+      {/* <HomeFooter /> */}
     </div>
   );
 }
--- src/components/shared/HomeNavbar.tsx
+++ src/components/shared/HomeNavbar.tsx
@@ -1,32 +1,39 @@
-'use  client';
+'use client';
 
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Button, Collapse, IconButton, Navbar, Typography } from '@material-tailwind/react';
 
 interface NavItemPropsType {
   label: string;
+  href: string;
 }
 
-function NavItem({ label }: NavItemPropsType) {
+function NavItem({ label, href }: NavItemPropsType) {
   return (
-    <a href="#">
-      <Typography as="li" color="blue-gray" className="p-1 font-medium text-black text-left">
-        {label}
-      </Typography>
-    </a>
+    <li>
+      <Link href={href}>
+        <Typography
+          as="a"
+          color="blue-gray"
+          className="p-1 font-medium text-black text-left hover:text-blue-500 transition-colors">
+          {label}
+        </Typography>
+      </Link>
+    </li>
   );
 }
 
 function NavList() {
   return (
     <ul className="mb-4 mt-2 flex flex-col items-start justify-start gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:items-start lg:gap-8 text-black">
-      <NavItem label="À propos" />
-      <NavItem label="Nos services" />
-      <NavItem label="Nous contacter" />
+      <NavItem label="À propos" href="/a-propos" />
+      <NavItem label="Nos services" href="/services" />
+      <NavItem label="Nous contacter" href="/contact" />
     </ul>
   );
 }
@@ -42,13 +49,9 @@ export function HomeNavbar() {
   return (
     <Navbar color="transparent" fullWidth>
       <div className="container mx-auto flex items-center justify-between text-black">
-        <Typography
-          as="a"
-          href="#"
-          color="blue-gray"
-          className="mr-4 cursor-pointer text-lg font-bold">
+        <Typography as="div" color="blue-gray" className="mr-4 cursor-pointer text-lg font-bold">
           <div style={{ maxWidth: '200px' }}>
-            <a href="/">
+            <Link href="/">
               <Image
                 src="/Gerico2.svg"
                 alt="Gerico Logo"
@@ -56,15 +59,16 @@ export function HomeNavbar() {
                 width={4}
                 height={1}
               />
-            </a>
+            </Link>
           </div>
         </Typography>
         <div className="hidden lg:block">
           <NavList />
         </div>
         <Button color="gray" className="hidden lg:inline-block bg-brand-300 normal-case">
-          Se connecter
+          <Link href="/auth/se-connecter/">Se connecter</Link>
         </Button>
+
         <IconButton
           size="sm"
           variant="text"
@@ -81,12 +85,12 @@ export function HomeNavbar() {
 
       <Collapse open={open}>
         <div
-          className={`mt-2 py-2 px-4 ${open ? 'bg-transparent' : 'bg-transparent'} ${
-            open ? 'rounded-none' : 'rounded-none'
-          } flex flex-col justify-start items-start`}>
+          className={`mt-2 py-2 px-4 ${
+            open ? 'bg-transparent' : 'bg-transparent'
+          } ${open ? 'rounded-none' : 'rounded-none'} flex flex-col justify-start items-start`}>
           <NavList />
           <Button className="mb-2 text-white bg-brand-300 flex justify-start normal-case">
-            Se connecter
+            <Link href="/auth/se-connecter/">Se connecter</Link>
           </Button>
         </div>
       </Collapse>
